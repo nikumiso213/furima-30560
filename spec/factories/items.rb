7b47c25d8dbb2a81527dfe02816e225b34661335
nikumiso213 <k1k3n2nikumiso213@gmail.com>
2020-10-25 19:36:20 +0900

    itemモデル単体テストコード実装

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,16 @@
 FactoryBot.define do
   factory :item do
-    
+    association :user
+    name                { Faker::Men.name}
+    description         { Faker::Men.text}
+    price               {1000}
+    delivery_id         {2}
+    product_status_id   {2}
+    category_id         {2}
+    shipping_days_id    {2}
+    area_id             {2}
+    after(:build) do |item|
+      item.image.attach(io: File.open('app/assets/images/flag.png'), filename: 'test.png', content_type: 'image/png')
+    end
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,93 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:item)
+  end
+
+  describe '商品出品機能の単体テスト' do
+    context '出品がうまくいくとき' do
+      it '9個の入力項目(name,description,category,product_status,delivery,area,shipping_days,price,user)が存在すれば登録できる' do
+        expect(@item).to be_valid
+      end
+    end
+
+    context '出品がうまくいかない時' do
+      it 'nameが空だと登録できない' do
+        @item.name = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Name can't be blank")
+      end
+      it 'descriptionが空だと登録できない' do
+        @item.description = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Description can't be blank")
+      end
+      it 'categoryが空だと登録できない' do
+        @item.category_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category can't be blank")
+      end
+      it 'カテゴリーが--だと登録できない' do
+        @item.category_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include('カテゴリーを選んでください')
+      end
+      it '商品状態が空だと登録できない' do
+        @item.product_status_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Product status can't be blank")
+      end
+      it '商品の状態が--だと登録できない' do
+        @item.product_status_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include('商品の状態を選んでください')
+      end
+      it '配送料が空だと登録できない' do
+        @item.delivery_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Delivery can't be blank")
+      end
+      it '配送料が--だと登録できない' do
+        @item.delivery_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include('配送料を選んでください')
+      end
+      it '配送元の地域が空だと登録できない' do
+        @item.area_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Area can't be blank")
+      end
+      it '配送元の地域が--だと登録できない' do
+        @item.area_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("配送元の地域を選んでください")
+      end
+      it '配送までの日数が空だと登録できない' do
+        @item.shipping_days_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping days can't be blank")
+      end
+      it '配送までの日数が--だと登録できない' do
+        @item.shipping_days_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("配送までの日数を選んでください")
+      end
+      it 'priceが空だと登録できない' do
+        @item.price = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price can't be blank", 'Price is not a number')
+      end
+      it 'priceが300より小さいと登録できない' do
+        @item.price = 299
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be greater than 299")
+      end
+      it 'priceが99999999より大きいと登録できない' do
+        @item.price = 10_000_000
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be less than 10000000")
+      end
+    end
+  end
 end
